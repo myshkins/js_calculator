71faf6c9193b2ac5bddffd6fbe7bae6f7144361f
mrpoopy <junk@krenitsky.org>
2022-09-19 08:20:28 -0400

    minor bug fixes

--- script.js
+++ script.js
@@ -4,58 +4,83 @@ let operator = "";
 let operand2 = "";
 let firstOp = true;
 let eq_pressed = false;
-
+const numCodes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+const opCodes = [173, 191, 61, ]
 
 const simpInputs = Array.from(document.querySelectorAll('.simp-input'));
-simpInputs.forEach(input => input.addEventListener('click', (e) => numberInput(e)));
+simpInputs.forEach(input => input.addEventListener('click', (e) => getNumInput(e)));
 
 const operators = Array.from(document.querySelectorAll('.operator'));
-operators.forEach(input => input.addEventListener('click', (e) => operatorInput(e)));
+operators.forEach(input => input.addEventListener('click', (e) => getOperatorInput(e)));
 
 const clearBtn = document.querySelector('#clear');
 clearBtn.addEventListener('click', clear);
 
 const equals = document.querySelector('#equals');
 equals.addEventListener('click', calculate);
+equals.addEventListener('click', eqPresser);
 
 const resultScreen = document.querySelector('.result-screen');
 const operatorScreen = document.querySelector('#operator-screen');
 
+window.addEventListener('keydown', keyInput);
 
-function numberInput(e) {
-    function inputter(operand) {
-        if (e.target.textContent === "0" && operand === "") {
-            return;
-        } else if (e.target.textContent === "." && operand.includes(".")) {
-            return;
-        } else {
-            // if (operand.split()[0] === "0") {
-            //     operand = operand.split().shift().join('') // remove a leading zero if there is one
-            // }
-            operand += e.target.textContent;
-            resultScreen.textContent = operand;
-            return operand;  
-        }}; 
+function keyInput(e) {
+    const keyValue = e.key
+    const keyCode = e.keyCode;
+    if (numCodes.includes(keyCode)){
+        numberInput
+    }
+    console.log(keyCode);
+    console.log(keyValue);
+}
 
+function getNumInput(e) {
+    const input = e.target.textContent;
+    numberInput(input);
+}
+
+function numberInput(input) {
     if (firstOp || eq_pressed) {
         if (eq_pressed) {
             operand1 = "";
             eq_pressed = false;
+            firstOp = true;
         }
         operand1 = inputter(operand1);
     } else {
         operand2 = inputter(operand2);   
     }
+
+    function inputter(operand) {
+        if (input === "0" && operand === "") {
+            return;
+        } else if (input === "." && operand.includes(".")) {
+            return;
+        } else {
+            // if (operand.split()[0] === "0") {
+            //     operand = operand.split().shift().join('') // remove a leading zero if there is one
+            // }
+            operand += input;
+            resultScreen.textContent = operand;
+            return operand;  
+        }}; 
 };
 
-function operatorInput(e) {
+function getOperatorInput(e) {
+    const input = e.target.textContent;
+    operatorInput(input);
+}
+
+function operatorInput(input) {
     if (operand2 != "") {
         operand1 = calculate();
-        operand2 = ""
+        operand2 = "";
+        firstOp = false;
     }
-    operator = e.target.textContent;
+    operator = input;
     firstOp = false;
-    operatorScreen.textContent = operator
+    operatorScreen.textContent = operator;
 }
 
 function calculate() {
@@ -77,13 +102,18 @@ function calculate() {
     operand2 = ""
     resultScreen.textContent = result;
     operatorScreen.textContent = ""
-    eq_pressed = true;
     return result;
+}
+
+function eqPresser() {
+    eq_pressed = true;
 }
 
 function clear() {
     operand1 = "";
     operand2 = "";
+    firstOp = true;
+    eq_pressed = false;
     resultScreen.textContent = "0"
     operatorScreen.textContent = ""
 }
